9400726763f9d6f0c3816b5c4ef5efa4f9ad7973
alsongard <alsongard@gmail.com>
2024-11-17 01:46:55 +0300

    Record App added

---
+++ Records/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Record(models.Model):
+    first_name = models.CharField(max_length=35)
+    middle_name = models.CharField(max_length=35)
+    last_name = models.CharField(max_length=35)
+    date_of_birth = models.DateField()
+    date_of_death = models.DateField()
+    content = models.TextField()
+    author = models.CharField(max_length=35)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
