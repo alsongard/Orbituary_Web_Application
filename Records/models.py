7f1ae2a5e70f4c1a8c45ec4f98cce7744dbec6ca
alsongard <alsongard@gmail.com>
2024-11-17 03:07:16 +0300

    record view changes

--- Records/models.py
+++ Records/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Record(models.Model):
     first_name = models.CharField(max_length=35)
-    middle_name = models.CharField(max_length=35)
+    middle_name = models.CharField(max_length=35, null=True, blank=True)
     last_name = models.CharField(max_length=35)
     date_of_birth = models.DateField()
     date_of_death = models.DateField()
