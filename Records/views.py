e9fa278e168399f1f235bbefcd03d724d1cc7444
alsongard <alsongard@gmail.com>
2024-11-17 03:48:48 +0300

    record_create_view created

--- Records/views.py
+++ Records/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-
+from django import forms
 
 from .models import Record
-
+from .form import RecordForm
 # Create your views here.
 def home_view(request):
     return render (request, "home.html", {})
@@ -13,3 +13,14 @@ def record_view(request):
         'object': obj
     }
     return render (request, "records/recordView.html", context)
+
+def records_create_view(request):
+    form = RecordForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        form = RecordForm()
+    context = {
+        'form': form
+    }
+
+    return render(request, 'records/recordData.html', context)
