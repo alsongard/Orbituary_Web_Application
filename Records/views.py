c394977d40f2f1044112731f1bd39c6171de0907
alsongard <alsongard@gmail.com>
2024-11-17 03:07:39 +0300

    record view done and working

--- Records/views.py
+++ Records/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+
+from .models import Record
+
 # Create your views here.
 def home_view(request):
     return render (request, "home.html", {})
 
-# def record_view(request):
-#     return render (request, "recordView.html", {})
+def record_view(request):
+    obj = Record.objects.get(id=1)
+    context = {
+        'object': obj
+    }
+    return render (request, "records/recordView.html", context)
